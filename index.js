669415fc8ddf4a2f92e707dd9e85a38942d3dcfa
Tumaini Muriithi <wendomuriithi@gmail.com>
2023-10-08 16:56:06 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "Jimmy",
+    streetAddress: "12 Broadway",
+
+};
+
+function updateEmployeeWithKeyAndValue(empl, ky, val) {
+    let newemploy = {...empl};
+    newemploy.ky = val;
+    return newemploy;
+
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(empl, ky, val) {
+    empl.ky = val;
+    return empl;
+
+}
+
+function deleteFromEmployeeByKey(empl, ky) {
+    let newemploy = {...empl};
+    delete newemploy.ky;
+    return newemploy;
+}
+
+function destructivelyDeleteFromEmployeeByKey(empl, ky) {
+    delete empl.ky;
+    return empl;
+}
